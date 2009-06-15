440ad83bc16053b6b2b4d205208e23c5d2413004
crc <crc@8419f86c-3064-4ce6-88eb-e955e58d3ca3>
2009-06-15 15:47:01 +0000

    Check-point expansion of Subscription
    git-svn-id: file:///home/crc/projects/Apollo/trunk@20 8419f86c-3064-4ce6-88eb-e955e58d3ca3

--- src/utils/fsm.py
+++ src/utils/fsm.py
@@ -5,6 +5,7 @@ Skip Montanaro's FSM (from http://wiki.python.org/moin/FiniteStateMachine)
 '''
 
 # Imports
+import xml.parsers.expat
 
 # Local Variables
 # Global Variables
@@ -64,6 +65,103 @@ class FSM:
    def debug(self, out):
       '''Assign a writable file to log transitions.'''
       self.dbg = out
+
+class ExpatFsm:
+   '''
+   A (little) less generic FSM.
+   Meant for internal use but might be relocate-able to utils package
+   '''
+   __ACQ_SIGNAL = 'start'
+
+   def __init__(self, tags, complete_cb):
+      '''Initialize with tag list and proc'''
+      # Parameterize Expat
+      self.__parser_init()
+
+      # (tag:value) mapping returned via callback/proc
+      self._tagseq = tags
+      self._acquire = dict.fromkeys(tags)
+      self._current_tag = iter(tags)
+
+      self.__fsm_init()
+      self.__callback = complete_cb
+
+   def __parser_init(self):
+      '''Help initialize Expat'''
+      self._parser = xml.parsers.expat.ParserCreate('utf-8')
+      self._parser.buffer_text = True
+      self._parser.StartElementHandler = self.start_element
+      self._parser.EndElementHandler = self.end_element
+      self._parser.CharacterDataHandler = self.char_data
+
+   def __fsm_init(self):
+      '''Helper initialize FSM'''
+      self._fsm = FSM()
+
+      tagiter = iter(self._tagseq)
+      init_tag = current_tag = tagiter.next()
+      while True:
+         try:
+            # Create distinct state transitions
+            acq_tag = '%s%s' % (current_tag, '_acq')
+
+            # Don't do anything before being signaled
+            self._fsm.add(current_tag, None, current_tag, None)
+            self._fsm.add(current_tag, self.__ACQ_SIGNAL, acq_tag, None)
+
+            try:
+               # Next user-/tag-defined state
+               current_tag = tagiter.next()
+               self._fsm.add(acq_tag, None, current_tag, self.acquire)
+            except StopIteration:
+               # Or close the loop
+               self._fsm.add(acq_tag, None, init_tag, self.last_action)
+               break
+         except StopIteration:
+            # TODO: Log: this should never happen
+            break
+
+      self._fsm.start(init_tag)
+
+   def parse(self, raw_xml):
+      '''Direct call on Expat instance'''
+      try:
+         self._parser.Parse(raw_xml, 1)
+      except xml.parsers.expat.ExpatError, err:
+         # TODO: Error path/Exceptions 
+         message = 'Expat Error: %s (%i, %i)' % (xml.parsers.expat.ErrorString(err.code),\
+                                                 err.lineno,\
+                                                 err.offset)
+
+   def start_element(self, name, attrs):
+      try:
+         if self._acquire.has_key(attrs['name']):
+            self._fsm.execute(self.__ACQ_SIGNAL)
+      except KeyError:
+         # No a problem: Don't Care tag
+         pass
+
+   def end_element(self, name):
+      pass
+
+   def char_data(self, data):
+      self._fsm.execute(data)
+
+   def acquire(self, state, input):
+      try:
+         current_key = self._current_tag.next()
+      except StopIteration:
+         # Reset Current Tag
+         self._current_tag = iter(self._tagseq)
+         current_key = self._current_tag.next()
+         pass
+
+      self._acquire[current_key] = input
+
+   def last_action(self, state, input):
+      self.acquire(state, input)
+      if self.__callback != None:
+         self.__callback(self._acquire)
 
 # Global Function Declarations
 def test():
