aa22d7486f0a8823d5fde2dfd5ffc717f64263cc
crc <crc@8419f86c-3064-4ce6-88eb-e955e58d3ca3>
2009-06-15 15:46:48 +0000

    Check-point expansion of Subscription
    git-svn-id: file:///home/crc/projects/Apollo/trunk@19 8419f86c-3064-4ce6-88eb-e955e58d3ca3

--- src/feeds/google/reader/list.py
+++ src/feeds/google/reader/list.py
@@ -6,10 +6,9 @@ Represent the 'List API' defined by pyrfeed (http://code.google.com/p/pyrfeed/wi
 
 # Imports
 import urllib
-import xml.parsers.expat
 
 from feeds.google.reader import access
-from utils.fsm import FSM
+from utils.fsm import ExpatFsm
 
 # Global Variables
 
@@ -24,20 +23,22 @@ class Subscription:
    '''Encapsulate for users of this module'''
    ID = 'id'
    TITLE = 'title'
+   COUNT = 'count'
 
    def __init__(self, tag_value_dict):
       #TODO: Is copy() necessary? How smart is the pointer?
       self._tag_value_dict = tag_value_dict.copy()
 
    def id(self):
-      return self.__tagvalue('id')
+      return self.__tagvalue(self.ID)
 
    def title(self):
-      return self.__tagvalue('title')
+      return self.__tagvalue(self.TITLE)
+
+   def count(self):
+      return self.__tagvalue(self.COUNT)
 
    def __tagvalue(self, tagname):
-      value = ''
-
       try:
          value = self._tag_value_dict[tagname]
       except KeyError:
@@ -47,196 +48,14 @@ class Subscription:
 
       return value
 
-class ExpatFsm:
-   '''
-   A (little) less generic FSM.
-   Meant for internal use but might be relocate-able to utils package
-   '''
-   __ACQ_SIGNAL = 'start'
-   __callback = None
-
-   def __init__(self, tags, complete_cb):
-      '''Initialize with tag list and proc'''
-      # Parameterize Expat
-      self.__parser_init()
-
-      # (tag:value) mapping returned via callback/proc
-      self._acquire = dict.fromkeys(tags)
-      self._current_tag = self._acquire.iterkeys()
-
-      self.__fsm_init()
-      self.__callback = complete_cb
-
-   def __parser_init(self):
-      '''Help initialize Expat'''
-      self._parser = xml.parsers.expat.ParserCreate('utf-8')
-      self._parser.buffer_text = True
-      self._parser.StartElementHandler = self.start_element
-      self._parser.EndElementHandler = self.end_element
-      self._parser.CharacterDataHandler = self.char_data
-
-   def __fsm_init(self):
-      '''Helper initialize FSM'''
-      self._fsm = FSM()
-
-      tagiter = self._acquire.iterkeys()
-      init_tag = current_tag = tagiter.next()
-      while True:
-         try:
-            # Create distinct state transitions
-            acq_tag = '%s%s' % (current_tag, '_acq')
-
-            # Don't do anything before being signaled
-            self._fsm.add(current_tag, None, current_tag, None)
-            self._fsm.add(current_tag, self.__ACQ_SIGNAL, acq_tag, None)
-
-            try:
-               # Next user-/tag-defined state
-               current_tag = tagiter.next()
-               self._fsm.add(acq_tag, None, current_tag, self.acquire)
-            except StopIteration:
-               # Or close the loop
-               self._fsm.add(acq_tag, None, init_tag, self.last_action)
-               break
-         except StopIteration:
-            # TODO: Log: this should never happen
-            break
-
-      self._fsm.start(init_tag)
-
-   def parse(self, raw_xml):
-      '''Direct call on Expat instance'''
-      try:
-         self._parser.Parse(raw_xml, 1)
-      except xml.parsers.expat.ExpatError, err:
-         # TODO: Error path/Exceptions 
-         message = 'Expat Error: %s (%i, %i)' % (xml.parsers.expat.ErrorString(err.code),\
-                                                 err.lineno,\
-                                                 err.offset)
-
-   def start_element(self, name, attrs):
-      try:
-         if self._acquire.has_key(attrs['name']):
-            self._fsm.execute(self.__ACQ_SIGNAL)
-      except KeyError:
-         # No a problem: Don't Care tag
-         pass
-
-   def end_element(self, name):
-      pass
-
-   def char_data(self, data):
-      self._fsm.execute(data)
-
-   def acquire(self, state, input):
-      try:
-         current_key = self._current_tag.next()
-      except StopIteration:
-         # Reset Current Tag
-         self._current_tag = self._acquire.iterkeys()
-         current_key = self._current_tag.next()
-         pass
-
-      self._acquire[current_key] = input
-
-   def last_action(self, state, input):
-      self.acquire(state, input)
-      self.__callback(self._acquire)
-
-class Subscriptions():
-   ''' Convenient packaging of List API (XML) output
-   '''
-   ID = 'id'
-   TITLE = 'title'
-   ACQUIRE = {'id' : 0, 'title' : 1}
-   ACQ_SIGNAL = 'start'
-
-   def __init__(self, raw_xml):
-      ''' Process XML now or later
-      '''
-      self._parser = xml.parsers.expat.ParserCreate('utf-8')
-      self._parser.buffer_text = True
-
-      self._parser.StartElementHandler = self.start_element
-      self._parser.EndElementHandler = self.end_element
-      self._parser.CharacterDataHandler = self.char_data
-
-      self._fsm = FSM()
-
-      self._fsm.add(self.ID, None, self.ID, None)
-      self._fsm.add(self.ID, self.ACQ_SIGNAL, self.ACQUIRE[self.ID], self.add_sub)
-      self._fsm.add(self.ACQUIRE[self.ID], None, self.TITLE, self.acquire_id)
-
-      self._fsm.add(self.TITLE, None, self.TITLE, None)
-      self._fsm.add(self.TITLE, self.ACQ_SIGNAL, self.ACQUIRE[self.TITLE], None)
-      self._fsm.add(self.ACQUIRE[self.TITLE], None, self.ID, self.acquire_title)
-
-      self._fsm.start(self.ID)
-
-      # List of Subscriptions
-      self._subs = []
-
-      try:
-         self._parser.Parse(raw_xml, 1)
-      except xml.parsers.expat.ExpatError, err:
-         message = 'Expat Error: %s (%i, %i)' % (xml.parsers.expat.ErrorString(err.code),\
-                                                 err.lineno,\
-                                                 err.offset)
-         # TODO: Error path/Exceptions 
-         return
-
-   def start_element(self, name, attrs):
-      attrs_vals = attrs.values()
-      if (self.ID in attrs_vals) or (self.TITLE in attrs_vals):
-         self._fsm.execute(self.ACQ_SIGNAL)
-
-   def end_element(self, name):
-      pass
-
-   def char_data(self, data):
-      self._fsm.execute(data)
-
-   def default_char_data(self, data):
-      ''' Default Character Data handler'''
-      self._fsm.execute(data)
-
-   def get_feed(self, title=None):
-      ''' Get Feed by title or all Feeds
-      '''
-      if not self._subs or title == None:
-         return self._subs
-
-      matching = []
-      for sub in self._subs:
-         if title in sub[self.TITLE]:
-            matching.append(sub)
-
-      return matching
-
-   def acquire_id(self, state, input):
-      ''' Populate Subscription List '''
-      sub = self._subs.pop()
-      sub[self.ID] = input
-      self._subs.append(sub)
-
-   def acquire_title(self, state, input):
-      ''' Populate Subscription List '''
-      sub = self._subs.pop()
-      sub[self.TITLE] = input
-      self._subs.append(sub)
-
-   def add_sub(self, state, input):
-      ''' Append to subscription list '''
-      sub = {self.ID : None, self.TITLE : None}
-      self._subs.append(sub)
-
 # Function Declarations
 def subscriptions(sub_xml=None):
    '''Call the proc with List of Subscriptions.'''
    if sub_xml == None:
       sub_xml = request(SUBSCRIPTIONS)
 
-   tags = ('id', 'title')
+   tags = (Subscription.ID,\
+           Subscription.TITLE)
    subs = []
 
    # Closure instantiates Subscription upon ExpatFsm event
@@ -248,6 +67,25 @@ def subscriptions(sub_xml=None):
    parser.parse(sub_xml)
 
    return subs
+
+def unread(raw_xml=None):
+   '''Call the proc with List of Subscriptions.'''
+   if raw_xml == None:
+      raw_xml = request(UNREAD_COUNT)
+
+   tags = (Subscription.ID,\
+           Subscription.COUNT)
+   unread = []
+
+   # Closure instantiates Subscription upon ExpatFsm event
+   def append(tag_dict):
+      anunread = Subscription(tag_dict)
+      unread.append(anunread)
+
+   parser = ExpatFsm(tags, append)
+   parser.parse(raw_xml)
+
+   return unread
 
 def request(api_id):
    ''' Helper formats request'''
--- src/feeds/google/reader/list_unit.py
+++ src/feeds/google/reader/list_unit.py
@@ -1,7 +1,7 @@
 '''
 Test Documentation
 '''
-from feeds.google.reader.list import Subscriptions
+import feeds.google.reader.list
 from feeds.google.reader.list import Subscription
 
 import xml.sax.saxutils
@@ -12,18 +12,20 @@ class Test(unittest.TestCase):
    def unescape(self):
        validate = [self.scoobie, self.snacks]
        for sub in validate:
-           sub[Subscriptions.ID] = xml.sax.saxutils.unescape(sub[Subscriptions.ID])
-           sub[Subscriptions.TITLE] = xml.sax.saxutils.unescape(sub[Subscriptions.TITLE])
+           sub[Subscription.ID] = xml.sax.saxutils.unescape(sub[Subscription.ID])
+           sub[Subscription.TITLE] = xml.sax.saxutils.unescape(sub[Subscription.TITLE])
 
        return validate
 
    def setUp(self):
-      self.scoobie = {Subscriptions.ID:'feed/http://www.scoobie.com&amp;_render=rss',\
-                 Subscriptions.TITLE:'Scoobie Subscription'}
-      self.snacks = {Subscriptions.ID:'feed/http://www.snacks.com', \
-                Subscriptions.TITLE:'Snacks Subscription'}
+      self.scoobie = {Subscription.ID:'feed/http://www.scoobie.com&amp;_render=rss',\
+                 Subscription.TITLE:'Scoobie Subscription',\
+                 Subscription.COUNT:'1000'}
+      self.snacks = {Subscription.ID:'feed/http://www.snacks.com', \
+                Subscription.TITLE:'Snacks Subscription',\
+                Subscription.COUNT:'500'}
 
-      self.testinput = """
+      self.sub_xml = """
       <object>
          <list name="subscriptions">
             <object>
@@ -41,27 +43,32 @@ class Test(unittest.TestCase):
                <number name="firstitemmsec">1237751512927</number>
             </object>
          </list>
-      </object>""" % (self.scoobie[Subscriptions.ID], self.scoobie[Subscriptions.TITLE],\
-                      self.snacks[Subscriptions.ID], self.snacks[Subscriptions.TITLE])
+      </object>""" % (self.scoobie[Subscription.ID], self.scoobie[Subscription.TITLE],\
+                      self.snacks[Subscription.ID], self.snacks[Subscription.TITLE])
+
+      self.unread_xml = """
+      <object>
+         <number name="max">1000</number>
+         <list name="unreadcounts">
+            <object>
+               <string name="id">%s</string>
+               <number name="count">%s</number>
+               <number name="newestItemTimestampUsec">1243356412327019</number>
+            </object>
+            <object>
+               <string name="id">%s</string>
+               <number name="count">%s</number>
+               <number name="newestItemTimestampUsec">1243329792089154</number>
+            </object>
+         </list>
+      </object>""" % (self.scoobie[Subscription.ID], self.scoobie[Subscription.COUNT],\
+                            self.snacks[Subscription.ID], self.snacks[Subscription.COUNT])
 
    def tearDown(self):
       pass
 
    def testSubscriptions(self):
-      subs = Subscriptions(self.testinput)
-
-      sublist = subs.get_feed()
-
-      elements = len(sublist)
-      self.assertEquals(elements, 2, 'Fail: Number of elements = %i' % elements)
-
-      validate = self.unescape()
-      for sub in sublist:
-         self.failUnless(sub in validate, 'Fail: Where is %s?' % sub)
-
-   def testExpatFsm(self):
-      import feeds.google.reader.list
-      subs = feeds.google.reader.list.subscriptions(self.testinput)
+      subs = feeds.google.reader.list.subscriptions(self.sub_xml)
 
       elements = len(subs)
       self.assertEquals(elements, 2, 'Number of elements = %i' % elements)
@@ -72,6 +79,19 @@ class Test(unittest.TestCase):
          valid = validiter.next()
          self.assertEquals(eachsub.id(), valid[Subscription.ID], 'Calculated ID = %s' % eachsub.id())
          self.assertEquals(eachsub.title(), valid[Subscription.TITLE], 'Calculated TITLE = %s' % eachsub.title())
+
+   def testUnread(self):
+      subs = feeds.google.reader.list.unread(self.unread_xml)
+
+      elements = len(subs)
+      self.assertEquals(elements, 2, 'Number of elements = %i' % elements)
+
+      validate = self.unescape()
+      validiter = iter(validate)
+      for eachsub in subs:
+         valid = validiter.next()
+         self.assertEquals(eachsub.id(), valid[Subscription.ID], 'Calculated ID = %s' % eachsub.id())
+         self.assertEquals(eachsub.count(), valid[Subscription.COUNT], 'Calculated COUNT = %s' % eachsub.count())
 
 suite = unittest.TestLoader().loadTestsFromTestCase(Test)
 unittest.TextTestRunner().run(suite)
