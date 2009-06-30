f7643fd252be4094b2a8de4dff1defb5233c8ba6
crc <crc@8419f86c-3064-4ce6-88eb-e955e58d3ca3>
2009-06-30 17:20:09 +0000

    Handling Forms with webapp
    git-svn-id: file:///home/crc/projects/Apollo/trunk@30 8419f86c-3064-4ce6-88eb-e955e58d3ca3

--- src/helloworld.py
+++ src/helloworld.py
@@ -3,6 +3,8 @@
 '''Test out AppEngine installation'''
 
 # Imports
+import cgi
+
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
@@ -12,16 +14,25 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 # Class Declarations
 class MainPage(webapp.RequestHandler):
    def get(self):
-      user = users.get_current_user()
+      self.response.out.write("""
+         <html>
+           <body>
+             <form action="/sign" method="post">
+               <div><textarea name="content" rows="3" cols="60"></textarea></div>
+               <div><input type="submit" value="Sign Guestbook"></div>
+             </form>
+           </body>
+         </html>""")
 
-      if user:
-         self.response.headers['Content-Type'] = 'text/plain'
-         self.response.out.write('Hello, ' + user.nickname())
-      else:
-         self.redirect(users.create_login_url(self.request.uri))
+class Guestbook(webapp.RequestHandler):
+  def post(self):
+    self.response.out.write('<html><body>You wrote:<pre>')
+    self.response.out.write(cgi.escape(self.request.get('content')))
+    self.response.out.write('</pre></body></html>')
 
 application = webapp.WSGIApplication(
-                                     [('/', MainPage)],
+                                     [('/', MainPage),
+                                      ('/sign', Guestbook)],
                                      debug=True)
 
 # Function Declarations
