fe49c2a7530b0b1e6e184c498c9d5af1ba65d587
crc <crc@8419f86c-3064-4ce6-88eb-e955e58d3ca3>
2009-07-02 00:03:52 +0000

    Using the Datastore
    git-svn-id: file:///home/crc/projects/Apollo/trunk@31 8419f86c-3064-4ce6-88eb-e955e58d3ca3

--- src/helloworld.py
+++ src/helloworld.py
@@ -8,27 +8,49 @@ import cgi
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import db
 
 # Global Variables
 
 # Class Declarations
+class Greeting(db.Model):
+  author = db.UserProperty()
+  content = db.StringProperty(multiline=True)
+  date = db.DateTimeProperty(auto_now_add=True)
+
 class MainPage(webapp.RequestHandler):
-   def get(self):
-      self.response.out.write("""
-         <html>
-           <body>
-             <form action="/sign" method="post">
-               <div><textarea name="content" rows="3" cols="60"></textarea></div>
-               <div><input type="submit" value="Sign Guestbook"></div>
-             </form>
-           </body>
-         </html>""")
+  def get(self):
+    self.response.out.write('<html><body>')
+
+    greetings = db.GqlQuery("SELECT * FROM Greeting ORDER BY date DESC LIMIT 10")
+
+    for greeting in greetings:
+      if greeting.author:
+        self.response.out.write('<b>%s</b> wrote:' % greeting.author.nickname())
+      else:
+        self.response.out.write('An anonymous person wrote:')
+      self.response.out.write('<blockquote>%s</blockquote>' %
+                              cgi.escape(greeting.content))
+
+    # Write the submission form and the footer of the page
+    self.response.out.write("""
+          <form action="/sign" method="post">
+            <div><textarea name="content" rows="3" cols="60"></textarea></div>
+            <div><input type="submit" value="Sign Guestbook"></div>
+          </form>
+        </body>
+      </html>""")
 
 class Guestbook(webapp.RequestHandler):
   def post(self):
-    self.response.out.write('<html><body>You wrote:<pre>')
-    self.response.out.write(cgi.escape(self.request.get('content')))
-    self.response.out.write('</pre></body></html>')
+    greeting = Greeting()
+
+    if users.get_current_user():
+      greeting.author = users.get_current_user()
+
+    greeting.content = self.request.get('content')
+    greeting.put()
+    self.redirect('/')
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
