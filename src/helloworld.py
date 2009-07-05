1ab225588e7d74c88796d764277a04b1c55007fd
crc <crc@8419f86c-3064-4ce6-88eb-e955e58d3ca3>
2009-07-05 23:27:27 +0000

    Integrate Termcloud sample page
    git-svn-id: file:///home/crc/projects/Apollo/trunk@34 8419f86c-3064-4ce6-88eb-e955e58d3ca3

--- src/helloworld.py
+++ src/helloworld.py
@@ -5,6 +5,8 @@
 # Imports
 import cgi
 import os
+import logging
+import datetime
 
 from google.appengine.ext.webapp import template
 from google.appengine.api import users
@@ -22,35 +24,48 @@ class Greeting(db.Model):
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-    greetings_query = Greeting.all().order('-date')
-    greetings = greetings_query.fetch(10)
+     try:
+        greetings_query = Greeting.all().order('-date')
+        greetings = greetings_query.fetch(10)
+     except:
+        logging.error('Error retrieving posts from the datastore')
 
-    if users.get_current_user():
-      url = users.create_logout_url(self.request.uri)
-      url_linktext = 'Logout'
-    else:
-      url = users.create_login_url(self.request.uri)
-      url_linktext = 'Login'
+     if users.get_current_user():
+        url = users.create_logout_url(self.request.uri)
+        url_linktext = 'Logout'
+     else:
+        url = users.create_login_url(self.request.uri)
+        url_linktext = 'Login'
 
-    template_values = {
-      'greetings': greetings,
-      'url': url,
-      'url_linktext': url_linktext,
-      }
+     template_values = {'greetings': greetings,
+                        'url': url,
+                        'url_linktext': url_linktext,
+                        }
 
-    path = os.path.join(os.path.dirname(__file__), 'index.html')
-    self.response.out.write(template.render(path, template_values))
+     path = os.path.join(os.path.dirname(__file__), 'index.html')
+     self.response.out.write(template.render(path, template_values))
 
 class Guestbook(webapp.RequestHandler):
-  def post(self):
-    greeting = Greeting()
+   def post(self):
+     logging.debug('Start guestbook signing request')
 
-    if users.get_current_user():
-      greeting.author = users.get_current_user()
+     greeting = Greeting()
 
-    greeting.content = self.request.get('content')
-    greeting.put()
-    self.redirect('/')
+     if users.get_current_user():
+        logging.info('Signed by user %s' % users.get_current_user().nickname())
+        greeting.author = users.get_current_user()
+     else:
+        logging.info('Signed by anonymous user')
+
+     greeting.content = self.request.get('content')
+     greeting.date = datetime.datetime.now()
+     try:
+        greeting.put()
+     except:
+        logging.error('There was an error saving comment %s' % self.request.get('content'))
+
+     logging.debug('Finish guestbook signing')
+     self.redirect('/')
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
@@ -59,8 +74,10 @@ application = webapp.WSGIApplication(
 
 # Function Declarations
 def main():
+   logging.getLogger().setLevel(logging.DEBUG)
    run_wsgi_app(application)
 
 # "main" body
 if __name__ == '__main__':
    main()
+
